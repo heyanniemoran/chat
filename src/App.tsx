e86e108604876c63bee3c692fa33cf20f7ecf7e9
heyanniemoran <heyanniemoran@gmail.com>
2022-08-22 00:13:24 +0300

    Добавила пакеты и иконки

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
+import '@fontsource/roboto';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
